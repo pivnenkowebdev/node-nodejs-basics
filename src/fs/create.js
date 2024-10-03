e6249443102811b112bd8c4c95a76efce56a5132
Anton Pivnenko <pivnenkowebdev@gmail.com>
2024-10-03 23:00:42 +0400

    feat: add the create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
 const create = async () => {
-    // Write your code here 
+    const folderPath = path.join('src', 'fs', 'files');
+    const fileName = 'fresh.txt';
+    const fullPath = path.join(folderPath, fileName);
+    const textContent = 'I am fresh and young';
+
+    try {
+        await fs.stat(fullPath);
+        throw new Error('FS operation failed');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            await fs.writeFile(fullPath, textContent);
+        } else {
+            console.log(error);
+        }
+    }
 };
 
 await create();
