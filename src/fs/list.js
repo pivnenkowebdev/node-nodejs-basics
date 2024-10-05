42e3b9f10937ee6fe01377079d7df75d8e9c0fb4
Anton Pivnenko <pivnenkowebdev@gmail.com>
2024-10-05 13:38:28 +0400

    feat: add list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
 const list = async () => {
-    // Write your code here 
+    const pathToFolder = path.join('src', 'fs', 'files');
+
+    try {
+        await fs.access(pathToFolder);
+        const textArr = await fs.readdir(pathToFolder);
+        console.log(textArr);
+
+        // // or this
+        // textArr.forEach(element => {
+        //     console.log(element);
+        // });
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.error('FS operation failed');
+        }
+    }
 };
 
 await list();
