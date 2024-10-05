13a321711f156f2f52f6db17e508835b3ebedfe5
Anton Pivnenko <pivnenkowebdev@gmail.com>
2024-10-05 13:00:23 +0400

    feat: add rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,32 @@
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+    const oldName = 'wrongFilename.txt';
+    const pathToWrongFile = path.join('src', 'fs', 'files', oldName);
+    
+    const newName = 'properFilename.md';
+    const pathToNewFile = path.join('src', 'fs', 'files', newName);
+
+    try {
+        await fs.access(pathToNewFile);
+        throw new Error('FS operation failed: has been created');
+    } catch (error) {
+        if (error.message === 'FS operation failed: has been created') {
+            console.error('FS operation failed: has been created');
+            return;
+        } else {
+            try {
+                await fs.access(pathToWrongFile);
+                await fs.rename(pathToWrongFile, pathToNewFile);
+            } catch (error) {
+                if (error.code === 'ENOENT') {
+                    console.error('FS operation failed: not found');
+                    throw new Error('FS operation failed');
+                }
+            }
+        }
+    }
 };
 
 await rename();
