be0264b6e19d99ef841f6dfb6f45ad5190ec2c12
Anton Pivnenko <pivnenkowebdev@gmail.com>
2024-10-05 13:11:33 +0400

    feat: add delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+    const pathToFile = path.join('src', 'fs', 'files', 'fileToRemove.txt');
+    try {
+        await fs.access(pathToFile);
+        await fs.unlink(pathToFile);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.error('FS operation failed: has been deleted');
+        }
+    }
 };
 
 await remove();
