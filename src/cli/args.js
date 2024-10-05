0de282ca87d6fd276a207f41a81605088b0c423a
Anton Pivnenko <pivnenkowebdev@gmail.com>
2024-10-05 22:06:42 +0400

    feat: add args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    
+    const prefixArg = '--';
+    const arrArgs = process.argv;
+    const resultArr = [];
+
+    for (let i = 0; i < arrArgs.length; i++) {
+        if (arrArgs[i].startsWith(prefixArg)) {
+            const keyWithoutDashed = arrArgs[i].replaceAll(prefixArg, '');
+            const finishedString = ` ${keyWithoutDashed} is ${arrArgs[i + 1]}`;
+            resultArr.push(finishedString);
+        }
+    }
+
+    console.log(resultArr.toString());
 };
 
 parseArgs();
