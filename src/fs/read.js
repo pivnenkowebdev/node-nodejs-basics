ec7d6077f686bc2544bca3b742c2df9d3c99e677
Anton Pivnenko <pivnenkowebdev@gmail.com>
2024-10-05 13:48:46 +0400

    feat: add read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
 const read = async () => {
-   
+    const pathToFile = path.join('src', 'fs', 'files', 'fileToRead.txt');
+
+    try {
+        await fs.access(pathToFile);
+        const content = await fs.readFile(pathToFile, 'utf8');
+        console.log(content);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.error('FS operation failed');
+            throw new Error('FS operation failed');
+        }
+    }
 };
 
 await read();
