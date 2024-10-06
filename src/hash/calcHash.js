b73f1b0f7f76910483d287dda25105545997f5b0
Anton Pivnenko <pivnenkowebdev@gmail.com>
2024-10-06 15:09:13 +0400

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,30 @@
-const calculateHash = async () => {
-    // Write your code here 
+import { createHmac } from 'node:crypto';
+import path from 'node:path';
+import fs from 'node:fs';
+
+const calculateHash  = async () => {
+    const pathToFile = path.join('src', 'hash', 'files', 'fileToCalculateHashFor.txt');
+    const key = 'yaustal';
+    const hash = createHmac('sha256', key);
+
+    return new Promise((resolve, reject) => {
+        const fileStream = fs.createReadStream(pathToFile);
+
+        fileStream.on('data', (chunk) => {
+            hash.update(chunk);
+        });
+
+        fileStream.on('end', () => {
+            const digest = hash.digest('hex');
+            console.log(digest);
+            resolve(digest);
+        });
+
+        fileStream.on('error', (error) => {
+            console.error('Error:', error);
+            reject(error);
+        });
+    });
 };
 
-await calculateHash();
+await calculateHash ();
