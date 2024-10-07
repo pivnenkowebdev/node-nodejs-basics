aeb7b0bc8a7ce62c1674a362dbeb1e5e4773fa1c
Anton Pivnenko <pivnenkowebdev@gmail.com>
2024-10-07 16:28:57 +0400

    feat: add write-streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import path from 'node:path';
+import fs from 'node:fs';
+
 const write = async () => {
-    // Write your code here 
+    const pathToFile = path.join('src', 'streams', 'files', 'fileToWrite.txt');
+    const writableStream = fs.createWriteStream(pathToFile);
+
+    process.stdin.setEncoding('utf-8');
+    process.stdin.on('data', (chunk) => {
+        writableStream.write(chunk);
+    })
+
+    process.stdin.on('end', () => {
+        writableStream.end();
+    })
 };
 
 await write();
